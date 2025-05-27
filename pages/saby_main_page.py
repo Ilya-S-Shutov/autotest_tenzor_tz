56d10e914d52ebc49880f3c63b791674891f2f69
Ilya-S-Shutov <ilya.s.shutov@gmail.com>
2025-05-27 15:38:46 +0500

    В POM главной страницы https://saby.ru добавлен метод перехода на страницу загрузок.

--- pages/saby_main_page.py
+++ pages/saby_main_page.py
@@ -8,6 +8,7 @@ from .base_page import BasePage
 class SabyMainPage(BasePage):
     CONTACTS_LOCATOR = (By.CSS_SELECTOR, "div.js-ContactsMenu")
     OFFICES_LOCATOR = (By.PARTIAL_LINK_TEXT, "офисов в регионе")
+    DOWNLOADS_LOCATOR = (By.LINK_TEXT, 'Скачать локальные версии')
 
     def __init__(self, driver: WebDriver, static_url: str = 'https://saby.ru'):
         super().__init__(driver, static_url)
@@ -18,3 +19,7 @@ class SabyMainPage(BasePage):
         self.click(self.CONTACTS_LOCATOR)
         self.click(self.OFFICES_LOCATOR)
 
+    @log_selenium_actions
+    def go_to_downloads(self):
+        """Перейти на страницу загрузок"""
+        self.click(self.DOWNLOADS_LOCATOR)
