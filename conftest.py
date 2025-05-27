e90d185763fad99e36bcfe70db7a2ec1f12c6d19
Ilya-S-Shutov <ilya.s.shutov@gmail.com>
2025-05-27 15:36:09 +0500

    В фикстуру, инициализирующую запуск браузера, добавлена инициализация параметров браузера.

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
 import logging
+import os
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -14,7 +16,17 @@ def pytest_addoption(parser):
 def driver(request):
     if request.config.getoption('--update_driver') == 'y':
         ChromeDriverManager.install()
-    my_driver = webdriver.Chrome()
+
+    chrome_options = Options()
+    prefs = {
+        "download.default_directory": os.path.join(os.getcwd(), 'tests'),
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True
+    }
+
+    chrome_options.add_experimental_option("prefs", prefs)
+    my_driver = webdriver.Chrome(options=chrome_options)
     my_driver.maximize_window()
     yield my_driver
     my_driver.quit()
